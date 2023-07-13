d6cd6c0058cfa32fca8d8a4eb0ca65d752335b6f
ManashX <manash20_ug@cse.nits.ac.in>
2023-07-13 23:58:43 +0530

    js

---
+++ script.js
@@ -0,0 +1,13 @@
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+const factList = document.querySelector('.fact-list');
+btn.addEventListener('click', function(){
+    if(form.classList.contains('hidden')){
+        form.classList.remove('hidden');
+        btn.textContent = 'Close';
+    }
+    else{
+        form.classList.add('hidden');
+        btn.textContent = 'Share a fact';
+    }
+})
